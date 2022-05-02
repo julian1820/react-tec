680a5abe61f644e1596898d476c69b55374a907a
julian1820 <pedrolozano99.j@gmail.com>
2022-05-02 20:10:01 -0300

    actualizar archivos

---
+++ src/components/NavBar.js
@@ -0,0 +1,70 @@
+import logo from '../logo.svg';
+import styled from 'styled-components'
+function NavBar() {
+    
+    let Header=styled.header`
+    background-image: linear-gradient(#001219, #001e2a);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 8px 8%;
+    `;
+
+let lis= {
+    fontfamily: 'Open Sans',
+    texttransform: 'uppercase',
+    fontsize: '1.1em',
+    color: '#f8f8f7',
+
+}
+
+let Li=styled.li` display:inline-block;
+padding: 0px 60px;
+text-decoration: none;`;
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: white;
+`;
+
+let log={
+    width: '100px',
+    height: '90px'
+}
+
+let navlinks={
+    transition: 'all 0.2s ease 0s'
+}
+const Button = styled.button`
+padding: 9px 30px;
+background-color: #0a9396;
+border: none;
+border-radius: 50px;
+cursor: pointer;
+transition: all 0.2s ease 0s;
+`;
+
+return (
+
+    <div id="navegador">
+    <Header >
+            <a style={lis} href="#"><img src={logo} style={log} className="App-logo" alt="logo" /></a>
+            <Title>Electronics Arts</Title>
+            <nav>
+                <ul className="nav-links" style={navlinks}>
+                    <Li style={lis} ><a href="#">Home</a></Li>
+                    <Li style={lis}><a href="#">Store</a></Li>
+                    <Li style={lis}><a href="#">Contact</a></Li>
+                </ul>
+            </nav>
+            <a style={lis} href="#"><Button >Sign Up</Button></a>
+        </Header>
+    </div>
+        );
+
+    };
+
+
+
+export default NavBar;
